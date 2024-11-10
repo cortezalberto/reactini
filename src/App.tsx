68300c198586e0c061782bac69cb2b312afb231c
cortezalberto <cortezalberto@gmail.com>
2024-11-10 17:53:28 -0300

    Segndo commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter";
-import { MIPrimerComponent } from "./components/MiPrimerComponent/MIPrimerComponent";
-import { ComponentUseEffect } from "./components/ComponentUseEffect/ComponentUseEffect";
-import { FormComponent } from "./components/FormComponent/FormComponent";
+
 import { AppProduct } from "./components/AppProduct/AppProduct";
 
 export const App = () => {
