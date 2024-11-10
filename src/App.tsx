8d01057e0bc799283af3e38ffa40d3ee0e3e726b
cortezalberto <cortezalberto@gmail.com>
2024-11-10 17:41:03 -0300

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter";
+import { MIPrimerComponent } from "./components/MiPrimerComponent/MIPrimerComponent";
+import { ComponentUseEffect } from "./components/ComponentUseEffect/ComponentUseEffect";
+import { FormComponent } from "./components/FormComponent/FormComponent";
+import { AppProduct } from "./components/AppProduct/AppProduct";
+
+export const App = () => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "2vh" }}>
+      {/* <MIPrimerComponent
+        text={"Texto desde propiedades"}
+        color="red"
+        fontSize={5}
+      />
+      <ComponentCounter />
+      <ComponentUseEffect /> */}
+      {/* <FormComponent /> */}
+      <AppProduct />
+    </div>
+  );
+};
